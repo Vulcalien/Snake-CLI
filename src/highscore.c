54a6505b0fce7229abedce7ca9c7b693f30a58a8
Vulcalien <vulcalien+github@gmail.com>
2021-05-11 10:26:55 +0200

    Copyright headers: removed the first empty line

--- src/highscore.c
+++ src/highscore.c
@@ -1,5 +1,4 @@
-/*
- * Copyright 2021 Vulcalien
+/* Copyright 2021 Vulcalien
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
